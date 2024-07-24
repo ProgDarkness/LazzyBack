8a11c57c5452c9afd8ae328882cb2c8fac1c01aa
Jeliuska <jeliuskatineo14@gmail.com>
2024-07-24 14:31:27 -0400

    prueba graphql

--- src/GraphQL/Resolvers/Prueba.js
+++ src/GraphQL/Resolvers/Prueba.js
@@ -1,10 +1,12 @@
 import { db1 } from '../../postgresdb'
 
 export default {
-    Query: {
-        consultaPrueba: async (_, __) => {
-            const entero = 0
-            return entero
-        }
+  Query: {
+    consultaPrueba: async (_, __) => {
+      const dataUser = await db1.manyOrNone(
+        'select * from public.d008t_usuarios'
+      )
+      return dataUser
     }
+  }
 }
