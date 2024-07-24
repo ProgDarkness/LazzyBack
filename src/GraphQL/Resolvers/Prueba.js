a8604e18aabc06ee4f1757b79be0816c84b51498
Jeliuska <jeliuskatineo14@gmail.com>
2024-07-24 13:25:54 -0400

    funcion de prueba

---
+++ src/GraphQL/Resolvers/Prueba.js
@@ -0,0 +1,10 @@
+import { db1 } from '../../postgresdb'
+
+export default {
+    Query: {
+        consultaPrueba: async (_, __) => {
+            const entero = 0
+            return entero
+        }
+    }
+}
